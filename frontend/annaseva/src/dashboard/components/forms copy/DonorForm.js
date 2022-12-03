b99847d9608b5c86efef4c00398fee0cb1ca96ce
deepak <deeepakjain132@gmail.com>
2022-12-03 15:33:25 +0530

    firebase setup

--- frontend/annaseva/src/dashboard/components/forms copy/DonorForm.js
+++ frontend/annaseva/src/dashboard/components/forms copy/DonorForm.js
@@ -1,65 +1,30 @@
 
 
 import React from 'react'
-import { useState } from 'react';
-import "./form.css"
-
-
 import {
     ref,
-    uploadBytesResumable, getDownloadURL
-} from "firebase/storage";
-import storage from '../../../firebase/firebaseConfig';
+    uploadBytesResumable
+    } from "firebase/storage";
 
+import { storage } from "../../../firebase/firebaseConfig";
 
 export const DonorForm = () => {
-    const [file, setFile] = useState("");
-    const [imgurl,setimgurl]=useState('');
+    const [file, setFile] = React.useState("");
 
-    // progress
-    const [percent, setPercent] = useState(0);
-
-    // Handle file upload event and update state
-    function handleChange(event) {
-        setFile(event.target.files[0]);
+// Handles input change event and updates state
+function handleChange(event) {
+setFile(event.target.files[0]);
+function handleUpload() {
+    if (!file) {
+    alert("Please choose a file first!")
     }
-
-    const handleUpload = () => {
-        if (!file) {
-            alert("Please upload an image first!");
-        }
-
-        const storageRef = ref(storage, `/files/${file.name}`);
-
-        // progress can be paused and resumed. It also exposes progress updates.
-        // Receives the storage reference and the file to upload.
-        const uploadTask = uploadBytesResumable(storageRef, file);
-
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                const percent = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
-
-                // update progress
-                setPercent(percent);
-            },
-            (err) => console.log(err),
-            () => {
-                // download url
-                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                    setimgurl(url)
-                    console.log(url);
-                });
-            }
-        );
-    };
-
+    }
+    const storageRef = ref(storage, `/files/${file.name}`);
+    
+}
     return (
         <div className='donorForm'>
-            <div className='form-css'>
-
+            <form>
                 <div className="form-row">
                     <div className="form-group col-md-6">
                         <label for="inputEmail4">Name of organization</label>
@@ -71,9 +36,9 @@ export const DonorForm = () => {
                     </div>
                 </div>
                 <div className="form-group">
-                    <div className="form-group col-md-6">
-                        <label for="inputAddress">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+                <div className="form-group col-md-6">
+                    <label for="inputAddress">Address</label>
+                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
                     </div>
 
                 </div>
@@ -142,26 +107,14 @@ export const DonorForm = () => {
                             <option>...</option>
                         </select>
                     </div>
-                    <div>
-                        <input type="file" onChange={handleChange} accept="/image/*" />
-                        <button onClick={handleUpload}>Upload to Firebase</button>
-                        {/* <p>{percent} "% done"</p> */}
-                        
-
-                        {imgurl && <>
-                        <br />
-                        <br />
-                        <img className='image-height' src={imgurl} alt="" />
-                        
-                        </>}
+                    <div className="form-group col-md-4">
+                        <label for="inputZip">Upload your food image</label>
+                        <input type="file" accept="image/*" onChange={handleChange} className="form-control" id="inputZip" />
                     </div>
-
-
                 </div>
                 <br />
-                <button type="submit" className="btn btn-primary" onClick={handleUpload}>Submit</button>
-            </div>
-
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
